77d10c3d6083fd1845f43b5370623aacf496665e
Dawid Ziaja <dawidekziaja@gmail.com>
2023-04-18 07:05:12 +0100

    Final touches

--- io.h
+++ io.h
@@ -9,8 +9,4 @@ struct mapStruct{
 struct mapStruct* loadEnergy(const char*);
 unsigned int loadPairs(struct Graph* g, struct hashTable* ht, const char* fn);
 
-
-//This is the most straight forward way of returning the hashtable and the graph
-//from loadEnergy that I could think of. probably not the best.
-
 #endif /*_IO_H*/
