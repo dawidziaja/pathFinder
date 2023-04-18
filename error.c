62c5458cea594459716b67148a828979d3bd6584
Dawid Ziaja <dawidekziaja@gmail.com>
2023-04-18 08:15:17 +0100

    My final gift to Stuart.

--- error.c
+++
@@ -1,8 +0,0 @@
-#include "error.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-void errorHandler(const char* msg){
-	fprintf(stderr, "\n%s\nExiting due to error..\n", msg);
-	exit(1);
-}
--- error.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ERROR_H
-#define _ERROR_H
-
-void errorHandler(const char*);
-
-#endif
